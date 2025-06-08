5da207abe8e38c8b1b2a2eff77d43b32023be337
milad998 <myaralbwd20@gmail.com>
2025-06-08 16:51:59 +0300

    Update db.go

---
+++ BookManager/config/db.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+    "context"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+    "log"
+    "time"
+)
+
+var DB *mongo.Client
+
+func ConnectDB() *mongo.Client {
+    client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    err = client.Connect(ctx)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    DB = client
+    return DB
+}
+
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+    return client.Database("librarydb").Collection(collectionName)
+}
