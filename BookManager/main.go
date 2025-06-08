281bcb7586d7961cc8640223a041ec22b254bd44
milad998 <myaralbwd20@gmail.com>
2025-06-08 17:49:35 +0300

    Update main.go

--- BookManager/main.go
+++ BookManager/main.go
@@ -9,8 +9,8 @@ import (
 )
 
 func main() {
-    config.ConnectDB()
-    r := router.Router()
+    config.ConnectDB() // هذا يحفظ الاتصال في config.DB
+    r := router.Router() // تأكد أن Router() تستخدم config.DB داخليًا
     fmt.Println("📚 Library API running on port 8080")
     log.Fatal(http.ListenAndServe(":8080", r))
 }
